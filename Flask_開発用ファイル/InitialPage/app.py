19329ec9a95287bcbf2d32b5bc99779c5ad2d1b1
本城 夕祐也 <yuyahonjo@HonjoYuya-MacBookPro.local>
2023-09-06 14:42:38 +0900

    Python_APP

--- Flask_開発用ファイル/InitialPage/app.py
+++ Flask_開発用ファイル/InitialPage/app.py
@@ -1,9 +1,17 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask
+
+load_dotenv(verbose=True)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return 'HelloWorld!'
 
-app.run()
+if __name__ == '__main__':
+    app.run()
